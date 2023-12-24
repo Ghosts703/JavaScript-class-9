774248a44f297d4c39cfdc26741c49c259908db6
Ghosts703 <fk254945+GitHub@gmail.com>
2023-12-24 17:02:59 +0500

    new changes. 24 dec 2023

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 // JS Class 9 - Sir Saad - Farhan Ali
 
+//-----------------------------------------------------------------------------------------------
 for (var i = 1; i <= 100; i = i + 10) {
   for (var j = i; j < i + 10; j++) {
     document.write(j + " ");
   }
   document.write("<br>");
 }
+
+document.write("<br><br>");
+//-----------------------------------------------------------------------------------------------
+for (var i = 1; i <= 5; i++) {
+  for (var j = 1; j <= i; j++) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
+
+document.write("<br><br>");
+//-----------------------------------------------------------------------------------------------
+for (var i = 1; i <= 5; i++) {
+  for (var j = 5; j >= i; j--) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
+
+//Due to this code, whole document isn't diplaying on the browser. check this code later.
+// document.write("<br><br>");          
+// //-----------------------------------------------------------------------------------------------
+// for (var i = 1; i <= 10; i++) {
+//   for (var j = 10; j <= i; j--) {
+//     document.write(j + " ");
+//   }
+//   document.write("<br>");
+// }
+
